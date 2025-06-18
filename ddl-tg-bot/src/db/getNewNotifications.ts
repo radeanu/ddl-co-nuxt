fec6f5fb41d9768337e034e2595d206f5a4adc3f
radeanu <radeanu.vasile.md@gmail.com>
2025-06-18 10:23:47 +0400

    fix tg labels

--- ddl-tg-bot/src/db/getNewNotifications.ts
+++ ddl-tg-bot/src/db/getNewNotifications.ts
@@ -1,7 +1,8 @@
 import prisma from '@/prisma/prisma.js';
+import { CLEANING_TYPES, PLACE_TYPES, OTHER_SERVICES } from '@/common/index.js';
 
-export function getNewNotifications() {
-	return prisma.tgNotification.findMany({
+export async function getNewNotifications() {
+	const list = await prisma.tgNotification.findMany({
 		where: {
 			sent: false
 		},
@@ -13,5 +14,33 @@ export function getNewNotifications() {
 				}
 			}
 		}
+	});
+
+	return list.map((item) => {
+		const cl_type =
+			CLEANING_TYPES.find((c) => c.value === item.Order.cl_type)?.name ?? '-';
+		const area_type =
+			PLACE_TYPES.find((c) => c.value === item.Order.area_type)?.name ?? '-';
+
+		const services = item.Order.OrderServices.map((s) => {
+			const s_name = OTHER_SERVICES.find((it) => it.value === s.name)?.name ?? '-';
+
+			return {
+				...s,
+				name: s_name
+			};
+		});
+
+		const order = {
+			...item.Order,
+			cl_type,
+			area_type,
+			OrderServices: services
+		};
+
+		return {
+			...item,
+			Order: order
+		};
 	});
 }
