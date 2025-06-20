0514c01a33de6cb79d2ae6f9980c6041dadeccc7
radeanu <radeanu.vasile.md@gmail.com>
2025-06-20 08:21:39 +0400

    update icons

--- ddl-client/nuxt.config.ts
+++ ddl-client/nuxt.config.ts
@@ -34,11 +34,36 @@ export default defineNuxtConfig({
 			},
 			charset: 'utf-8',
 			viewport: 'width=device-width, initial-scale=1',
+			meta: [
+				{
+					name: 'apple-mobile-web-app-title',
+					content: 'DDL'
+				}
+			],
 			link: [
 				{
 					rel: 'icon',
+					type: 'image/png',
+					href: '/favicon-96x96.png',
+					sizes: '96x96'
+				},
+				{
+					rel: 'icon',
 					type: 'image/svg+xml',
-					href: '/images/svg/logo.svg'
+					href: '/favicon.svg'
+				},
+				{
+					rel: 'shortcut icon',
+					href: '/favicon.ico'
+				},
+				{
+					rel: 'apple-touch-icon',
+					sizes: '180x180',
+					href: '/apple-touch-icon.png'
+				},
+				{
+					rel: 'manifest',
+					href: '/site.webmanifest'
 				}
 			]
 		},
